cc3d12571d9551d921eda00fba83411b218fe07a
Marko <marko@milicevic.fr>
2024-06-24 21:07:31 +0100

    feat(ECF-26): Create GET /api/v1/sessions/:sessionId

--- backend/src/Session/routes.js
+++ backend/src/Session/routes.js
@@ -1,7 +1,11 @@
+import GetSessionController from "./Adapter/Http/GetSession/GetSessionController.js";
 import GetSessionsController from "./Adapter/Http/GetSessions/GetSessionsController.js";
+import GetSessionRepository from "./Adapter/Sequelize/GetSession/GetSessionRepository.js";
+import GetSessionReservedPlacementRepository from "./Adapter/Sequelize/GetSession/GetSessionReservedPlacementRepository.js";
 import GetSessionsCinemasRepository from "./Adapter/Sequelize/GetSessions/GetSessionsCinemasRepository.js";
 import GetSessionsPricesRepository from "./Adapter/Sequelize/GetSessions/GetSessionsPricesRepository.js";
 import GetSessionsRepository from "./Adapter/Sequelize/GetSessions/GetSessionsRepository.js";
+import GetSessionService from "./UseCase/GetSession/GetSessionService.js";
 import GetSessionsService from "./UseCase/GetSessions/GetSessionsService.js";
 
 export const loadSessionRoutes = (app) => {
@@ -14,6 +18,19 @@ export const loadSessionRoutes = (app) => {
 			const response = await controller.handle({
 				...req.query,
 				me: req.me || undefined,
+			});
+			res.status(200).json(response);
+		} catch (err) {
+			console.error(err);
+			res.status(500).json({ error: true });
+		}
+	});
+
+	app.get("/api/v1/sessions/:sessionId", async (req, res) => {
+		try {
+			const controller = new GetSessionController(null, new GetSessionService(new GetSessionRepository(), new GetSessionReservedPlacementRepository()));
+			const response = await controller.handle({
+				sessionId: req.params.sessionId,
 			});
 			res.status(200).json(response);
 		} catch (err) {
