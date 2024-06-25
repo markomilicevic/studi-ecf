6348f7423c395b093a0b38fdaa13cd9c38e2ea17
Marko <marko@milicevic.fr>
2024-06-25 06:34:26 +0100

    feat(ECF-32): Show signin and signout

--- website/src/components/organisms/AccountForm/AccountForm.jsx
+++ website/src/components/organisms/AccountForm/AccountForm.jsx
@@ -1,12 +1,16 @@
 import Grid from "@mui/material/Grid";
 
 import { SignupForm } from "components/organisms/SignupForm";
+import { SigninForm } from "components/organisms/SigninForm";
 
 export default function AccountForm({ connect }) {
 	return (
 		<Grid container spacing={8}>
 			<Grid item xs={12} md={6}>
 				<SignupForm onUserCreated={connect} />
+			</Grid>
+			<Grid item xs={12} md={6}>
+				<SigninForm onUserAuthentified={connect} />
 			</Grid>
 		</Grid>
 	);
--- website/src/components/pages/Account/Account.jsx
+++ website/src/components/pages/Account/Account.jsx
@@ -1,8 +1,20 @@
-import { Page } from "components/templates/Page";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
 import { AccountForm } from "components/organisms/AccountForm";
+import { Page } from "components/templates/Page";
+import { CurrentUserContext } from "components/templates/Page/providers/CurrentUserProvider";
 
 export default function Account() {
-	const connect = () => {};
+	const navigate = useNavigate();
+
+	const { flag } = useContext(CurrentUserContext);
+
+	const connect = () => {
+		flag(); // Create client's flag
+
+		navigate("/"); // HomePage
+	};
 
 	return (
 		<Page data-testid="account">
