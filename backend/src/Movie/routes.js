fcb5cd763bac673ff6c3e56cfdd19086aad90128
Marko <marko@milicevic.fr>
2024-06-26 05:47:44 +0100

    feat(ECF-35): Create DELETE /api/v1/movies/:movieId

--- backend/src/Movie/routes.js
+++ backend/src/Movie/routes.js
@@ -1,20 +1,23 @@
+import CreateMovieController from "./Adapter/Http/CreateMovie/CreateMovieController.js";
+import DeleteMovieController from "./Adapter/Http/DeleteMovie/DeleteMovieController.js";
 import GetActiveMoviesController from "./Adapter/Http/GetActiveMovies/GetActiveMoviesController.js";
 import GetMovieCommentsController from "./Adapter/Http/GetMovieComments/GetMovieCommentsController.js";
 import GetMoviesController from "./Adapter/Http/GetMovies/GetMoviesController.js";
 import UpdateMovieController from "./Adapter/Http/UpdateMovie/UpdateMovieController.js";
 import UploadMoviePosterController from "./Adapter/Http/UploadMoviePoster/UploadMoviePosterController.js";
+import CreateMovieRepository from "./Adapter/Sequelize/CreateMovie/CreateMovieRepository.js";
+import DeleteMovieRepository from "./Adapter/Sequelize/DeleteMovie/DeleteMovieRepository.js";
 import GetActiveMoviesRepository from "./Adapter/Sequelize/GetActiveMovies/GetActiveMoviesRepository.js";
 import GetMovieCommentsRepository from "./Adapter/Sequelize/GetMovieComments/GetMovieCommentsRepository.js";
 import GetMoviesRepository from "./Adapter/Sequelize/GetMovies/GetMoviesRepository.js";
 import UpdateMovieRepository from "./Adapter/Sequelize/UpdateMovie/UpdateMovieRepository.js";
+import CreateMovieService from "./UseCase/CreateMovie/CreateMovieService.js";
+import DeleteMovieService from "./UseCase/DeleteMovie/DeleteMovieService.js";
 import GetActiveMoviesService from "./UseCase/GetActiveMovies/GetActiveMoviesService.js";
 import GetMovieCommentsService from "./UseCase/GetMovieComments/GetMovieCommentsService.js";
 import GetMoviesService from "./UseCase/GetMovies/GetMoviesService.js";
 import UpdateMovieService from "./UseCase/UpdateMovie/UpdateMovieService.js";
 import UploadMoviePosterService from "./UseCase/UploadMoviePoster/UploadMoviePosterService.js";
-import CreateMovieController from "./Adapter/Http/CreateMovie/CreateMovieController.js";
-import CreateMovieService from "./UseCase/CreateMovie/CreateMovieService.js";
-import CreateMovieRepository from "./Adapter/Sequelize/CreateMovie/CreateMovieRepository.js";
 
 export const loadMovieRoutes = (app) => {
 	app.get("/api/v1/movies/actives", async (req, res) => {
@@ -105,6 +108,27 @@ export const loadMovieRoutes = (app) => {
 			const response = await controller.handle(req.body);
 
 			let code = 201; // Created
+			if (response.status === "USER_ERRORS") {
+				code = 400; // Bad request
+			}
+
+			res.status(code).json(response);
+		} catch (err) {
+			console.error(err);
+			res.status(500).json({ error: true });
+		}
+	});
+
+	app.delete("/api/v1/movies/:movieId", async (req, res) => {
+		try {
+			if (!req.me || !["admin", "employee"].includes(req.me.role)) {
+				return res.status(401).json({ error: true });
+			}
+
+			const controller = new DeleteMovieController(null, new DeleteMovieService(new DeleteMovieRepository()));
+			const response = await controller.handle({ movieId: req.params.movieId });
+
+			let code = 204; // No content
 			if (response.status === "USER_ERRORS") {
 				code = 400; // Bad request
 			}
