65014f092a3525867a2b2c0ac97c931d9259c6c0
Marko <marko@milicevic.fr>
2024-06-26 05:41:13 +0100

    feat(ECF-35): Create PUT /api/v1/movies/:movieId

--- backend/src/Movie/routes.js
+++ backend/src/Movie/routes.js
@@ -1,13 +1,16 @@
 import GetActiveMoviesController from "./Adapter/Http/GetActiveMovies/GetActiveMoviesController.js";
 import GetMovieCommentsController from "./Adapter/Http/GetMovieComments/GetMovieCommentsController.js";
 import GetMoviesController from "./Adapter/Http/GetMovies/GetMoviesController.js";
+import UpdateMovieController from "./Adapter/Http/UpdateMovie/UpdateMovieController.js";
+import UploadMoviePosterController from "./Adapter/Http/UploadMoviePoster/UploadMoviePosterController.js";
 import GetActiveMoviesRepository from "./Adapter/Sequelize/GetActiveMovies/GetActiveMoviesRepository.js";
 import GetMovieCommentsRepository from "./Adapter/Sequelize/GetMovieComments/GetMovieCommentsRepository.js";
 import GetMoviesRepository from "./Adapter/Sequelize/GetMovies/GetMoviesRepository.js";
+import UpdateMovieRepository from "./Adapter/Sequelize/UpdateMovie/UpdateMovieRepository.js";
 import GetActiveMoviesService from "./UseCase/GetActiveMovies/GetActiveMoviesService.js";
 import GetMovieCommentsService from "./UseCase/GetMovieComments/GetMovieCommentsService.js";
 import GetMoviesService from "./UseCase/GetMovies/GetMoviesService.js";
-import UploadMoviePosterController from "./Adapter/Http/UploadMoviePoster/UploadMoviePosterController.js";
+import UpdateMovieService from "./UseCase/UpdateMovie/UpdateMovieService.js";
 import UploadMoviePosterService from "./UseCase/UploadMoviePoster/UploadMoviePosterService.js";
 
 export const loadMovieRoutes = (app) => {
@@ -57,6 +60,27 @@ export const loadMovieRoutes = (app) => {
 			const response = await controller.handle(req.files);
 
 			let code = 201; // Created
+			if (response.status === "USER_ERRORS") {
+				code = 400; // Bad request
+			}
+
+			res.status(code).json(response);
+		} catch (err) {
+			console.error(err);
+			res.status(500).json({ error: true });
+		}
+	});
+
+	app.put("/api/v1/movies/:movieId", async (req, res) => {
+		try {
+			if (!req.me || !["admin", "employee"].includes(req.me.role)) {
+				return res.status(401).json({ error: true });
+			}
+
+			const controller = new UpdateMovieController(null, new UpdateMovieService(new UpdateMovieRepository()));
+			const response = await controller.handle({ ...req.body, movieId: req.params.movieId });
+
+			let code = 204; // No content
 			if (response.status === "USER_ERRORS") {
 				code = 400; // Bad request
 			}
