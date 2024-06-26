e4cc36649ae1f65e5ccdba6fea0faf14514d1f91
Marko <marko@milicevic.fr>
2024-06-26 18:21:14 +0100

    feat(ECF-36): Create DELETE /api/v1/cinemas/:cinemaId/rooms/:cinemaRoomId

--- backend/src/Cinema/routes.js
+++ backend/src/Cinema/routes.js
@@ -1,15 +1,18 @@
 import GetGeoJsClosestCinemaRepository from "./Adapter/GeoJs/GetClosestCinema/GetClosestCinemaRepository.js";
 import CreateCinemaRoomController from "./Adapter/Http/CreateCinemaRoom/CreateCinemaRoomController.js";
+import DeleteCinemaRoomController from "./Adapter/Http/DeleteCinemaRoom/DeleteCinemaRoomController.js";
 import GetCinemaRoomsController from "./Adapter/Http/GetCinemaRooms/GetCinemaRoomsController.js";
 import GetCinemasController from "./Adapter/Http/GetCinemas/GetCinemasController.js";
 import GetClosestCinemaController from "./Adapter/Http/GetClosestCinema/GetClosestCinemaController.js";
 import UpdateCinemaRoomController from "./Adapter/Http/UpdateCinemaRoom/UpdateCinemaRoomController.js";
 import CreateCinemaRoomRepository from "./Adapter/Sequelize/CreateCinemaRoom/CreateCinemaRoomRepository.js";
+import DeleteCinemaRoomRepository from "./Adapter/Sequelize/DeleteCinemaRoom/DeleteCinemaRoomRepository.js";
 import GetCinemaRoomsRepository from "./Adapter/Sequelize/GetCinemaRooms/GetCinemaRoomsRepository.js";
 import GetCinemasRepository from "./Adapter/Sequelize/GetCinemas/GetCinemasRepository.js";
 import GetSequelizeClosestCinemaRepository from "./Adapter/Sequelize/GetClosestCinema/GetClosestCinemaRepository.js";
 import UpdateCinemaRoomRepository from "./Adapter/Sequelize/UpdateCinemaRoom/UpdateCinemaRoomRepository.js";
 import CreateCinemaRoomService from "./UseCase/CreateCinemaRoom/CreateCinemaRoomService.js";
+import DeleteCinemaRoomService from "./UseCase/DeleteCinemaRoom/DeleteCinemaRoomService.js";
 import GetCinemaRoomsService from "./UseCase/GetCinemaRooms/GetCinemaRoomsService.js";
 import GetCinemasService from "./UseCase/GetCinemas/GetCinemasService.js";
 import GetClosestCinemaService from "./UseCase/GetClosestCinema/GetClosestCinemaService.js";
@@ -107,6 +110,30 @@ export const loadCinemaRoutes = (app) => {
 			const response = await controller.handle({ ...req.body, cinemaId: req.params.cinemaId });
 
 			let code = 201; // Created
+			if (response.status === "USER_ERRORS") {
+				code = 400; // Bad request
+			}
+
+			res.status(code).json(response);
+		} catch (err) {
+			console.error(err);
+			res.status(500).json({ error: true });
+		}
+	});
+
+	app.delete("/api/v1/cinemas/:cinemaId/rooms/:cinemaRoomId", async (req, res) => {
+		try {
+			if (!req.me || !["admin", "employee"].includes(req.me.role)) {
+				return res.status(401).json({ error: true });
+			}
+
+			const controller = new DeleteCinemaRoomController(null, new DeleteCinemaRoomService(new DeleteCinemaRoomRepository()));
+			const response = await controller.handle({
+				cinemaId: req.params.cinemaId,
+				cinemaRoomId: req.params.cinemaRoomId,
+			});
+
+			let code = 204; // No content
 			if (response.status === "USER_ERRORS") {
 				code = 400; // Bad request
 			}
